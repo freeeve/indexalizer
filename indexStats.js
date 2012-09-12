bc9e2630eb862fae36939d9156d4001c8438896b
Wes Freeman <freeman.wes@gmail.com>
2012-09-12 11:02:22 -0400

    Woops, I had been editing the wrong file..

--- indexStats.js
+++ indexStats.js
@@ -1,6 +1,8 @@
 DB.prototype.indexStats = function() {
   var queries = [];
   var collections = db.getCollectionNames();
+  // this could probably be made better, caching by index used instead of exact query 
+  // (because queries on _id for example can be all over the place)
   var findQuery = function(q) {
     for(entryIdx in queries) {
       if(q == queries[entryIdx].query) {
@@ -13,33 +15,33 @@ DB.prototype.indexStats = function() {
   for(cIdx in collections) {
     var cName = collections[cIdx];
     if(cName.indexOf("system") == -1) {
-      db.system.profile.find({ns:db.getName()+"."+cName}).limit(10).forEach(function(profileDoc) {
-        if(profileDoc.query != {}) { 
-        var qIdx = findQuery(profileDoc.query);
-        if(qIdx == -1) {
-          var size = queries.push({query:profileDoc.query, count:1, index:""});
-          var explain = db[cName].find(queries[size-1].query).explain();
-          if(profileDoc.query["query"]) {
-            queries[size-1].query = profileDoc.query['query'];
-            queries[size-1].sort = profileDoc.query['orderby'];
-            if(queries[size-1].sort) {
-              explain = db[cName].find(queries[size-1].query).sort(queries[size-1].sort).explain();
-              print("printing explain with sort: ");
-              printjson(explain);
+      var i = 1;
+      var count = db.system.profile.count({ns:db.getName()+"."+cName});
+      print("scanning profile with "+count+" records... this could take a while.");
+      db.system.profile.find({ns:db.getName()+"."+cName}).forEach(function(profileDoc) {
+        if(profileDoc.query && !profileDoc.query["$explain"]) { 
+          var qIdx = findQuery(profileDoc.query);
+          if(qIdx == -1) {
+            var size = queries.push({query:profileDoc.query, count:1, index:""});
+            var explain = db[cName].find(queries[size-1].query).explain();
+            if(profileDoc.query && profileDoc.query["query"]) {
+              queries[size-1].sort = profileDoc.query['orderby'];
+              if(queries[size-1].sort) {
+                explain = db[cName].find(queries[size-1].query.query).sort(queries[size-1].sort).explain();
+              }
             }
-          }
-          queries[size-1].cursor = explain.cursor;
-          queries[size-1].millis = explain.millis;
-          if(explain.cursor != "BasicCursor") {
-            queries[size-1].index = explain.cursor.split(" ")[1];
+            queries[size-1].cursor = explain.cursor;
+            queries[size-1].millis = explain.millis;
+            if(explain.cursor != "BasicCursor") {
+              queries[size-1].index = explain.cursor.split(" ")[1];
+              //print("found index in use: " + queries[size-1].index); 
+            } else {
+              print("warning, no index for query: ");
+              printjson(profileDoc.query);
+            }
           } else {
-            print("warning, no index for query: ");
-            printjson(profileDoc.query);
+            queries[qIdx].count++;
           }
-          printjson(explain);
-        } else {
-          queries[qIdx].count++;
-        }
         }
       });
     }
@@ -47,21 +49,23 @@ DB.prototype.indexStats = function() {
 
   for(cIdx in collections) {
     var cName = collections[cIdx];
-    print("checking indexes not used in: " + cName);
-    for(iIdx in db[cName].getIndexes()) {
-      var iName = db[cName].getIndexes()[iIdx].name;
-      print("checking if "+iName+" is being used..");
-      var stats = db[cName].stats();
-      var found = false;
-      for(qIdx in queries) {
-        if(queries[qIdx].idx == iName) {
-          found = true;
-          continue;
+    if(cName.indexOf("system") == -1) {
+      print("checking for unused indexes in: " + cName);
+      for(iIdx in db[cName].getIndexes()) {
+        var iName = db[cName].getIndexes()[iIdx].name;
+        if(iName.indexOf("system") == -1) {
+          var stats = db[cName].stats();
+          var found = false;
+          for(qIdx in queries) {
+            if(queries[qIdx].index == iName) {
+              found = true;
+            }
+          }
+          if(!found) {
+            print("this index is not being used: ");
+            printjson(iName);
+          }
         }
-      }
-      if(!found) {
-        print("this index is not being used: ");
-        printjson(iName);
       }
     }
   }
